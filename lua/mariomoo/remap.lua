cc04e6fcaea301c0132246508b7757279acce843
mariogarzac <mariogarzac99@gmail.com>
2023-03-16 10:48:04 -0600

    added key binding to open a terminal in a new tab

--- lua/mariomoo/remap.lua
+++ lua/mariomoo/remap.lua
@@ -7,6 +7,9 @@ vim.keymap.set("n", ",", ":")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<C-t>", ":tabnew | term \n")
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- Use ctrl-[hjkl] to select the active split!
