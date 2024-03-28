269a7535c83b4a42819a7aa026939bf7b86107ba
mariogarzac <mariogarzac99@gmail.com>
2024-03-28 12:05:42 -0600

    Removed commented part

--- lua/mariomoo/plugins/tresitter.lua
+++ lua/mariomoo/plugins/tresitter.lua
@@ -15,15 +15,5 @@ return {
                 additional_vim_regex_highlighting = false,
             },
         }
-
-        -- local treesitter_parser_config = require("nvim-tresitter.parsers").get_parser_configs() 
-        -- treesitter_parser_config.templ {
-            -- install_info = {
-                -- url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                -- files = {"src/parcer.c", "src/scanner.c"},
-                -- branch = "master",
-            -- }
-        -- }
-        -- vim.treesitter.language.register('templ', 'templ')
     end
 }
