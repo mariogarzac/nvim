3f41c67231e80e2c7892ef92a3f559996c4b626f
mariogarzac <mariogarzac99@gmail.com>
2024-03-28 12:06:05 -0600

    Added priority

--- lua/mariomoo/plugins/colors.lua
+++ lua/mariomoo/plugins/colors.lua
@@ -1,7 +1,7 @@
 -- function SetColors(color)
 	-- color = color or "tokyonight-storm",
 	-- vim.cmd.colorscheme(color),
--- 
+--
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
     -- vim.cmd('highlight SignColumn guibg=NONE')
@@ -10,8 +10,8 @@
 return {
     {
     "folke/tokyonight.nvim",
-    lazy = false, 
-    priority = 1000, 
+    lazy = false,
+    priority = 1001,
     config = function()
         require("tokyonight").setup({
             style = "storm",
